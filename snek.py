0e98de8cdfac4352dfae85bfca7e06388a943a94
SushiPython <github@sushipython.us>
2026-03-02 17:29:45 -0800

    snake movement

--- snek.py
+++ snek.py
@@ -4,36 +4,46 @@ from typing import Optional
 
 class SnakeGame:
     def __init__(self, width: int, height: int):
-        """
-        Game should be initialized according to width
-        and height. The game is played using wasd keys.
-
-        The initial state should be:
-        - Snake of length 3
-        - Snake moving right
-        - Snake in the center
-        """
-        pass
+        self.w = width
+        self.h = height
+        cx = width // 2
+        cy = height // 2
+        self.body = [(cy, cx - 2), (cy, cx - 1), (cy, cx)]
+        self.dx = 0
+        self.dy = 0
+        self.next_dx = 1
+        self.next_dy = 0
 
     def grid(self) -> list[list[str]]:
-        """
-        Handles the display of the grid
-        Should return a 2D list of characters
-        """
-        return []
+        g = [[" " for _ in range(self.w)] for _ in range(self.h)]
+        for y, x in self.body:
+            if 0 <= y < self.h and 0 <= x < self.w:
+                g[y][x] = "#"
+        return g
 
     def set_direction(self, key_input: Optional[str]):
-        """
-        Set the direction.
-        Assume input will either be w, a, s, d, or None
-        """
-        pass
+        if key_input == "d":
+            self.next_dx, self.next_dy = 1, 0
+        elif key_input == "a":
+            self.next_dx, self.next_dy = -1, 0
+        elif key_input == "w":
+            self.next_dx, self.next_dy = 0, -1
+        elif key_input == "s":
+            self.next_dx, self.next_dy = 0, 1
 
     def do_step(self) -> bool:
-        """
-        Performs a single step during 1 tick
-        Return False if the game should end
-        """
+        if self.next_dx != 0 or self.next_dy != 0:
+            self.dx = self.next_dx
+            self.dy = self.next_dy
+        head_y, head_x = self.body[-1]
+        ny = head_y + self.dy
+        nx = head_x + self.dx
+        if ny < 0 or ny >= self.h or nx < 0 or nx >= self.w:
+            return False
+        if (ny, nx) in self.body:
+            return False
+        self.body.append((ny, nx))
+        self.body.pop(0)
         return True
 
 
